9bf362671f9a0adf95cee8dcb497009d3779408e
MAbdurahman <mdbdrrhm@gmail.com>
2022-01-13 09:05:36 -0600

    Add basic routes in App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+import Home from './../pages/Home';
+import Contact from './../pages/Contact';
+import Courses from './../pages/Courses';
+import Projects from './../pages/Projects';
+import About from './../pages/About';
 
 
 export default function App() {
   return (
-    <div className='App'>
-      <h2>reactjs - portfolio</h2>
-    </div>
-  )
+		<div>
+			<BrowserRouter>
+				<Route path='/' exact component={Home} />
+        <Route path='/about' exact component={About} />
+				<Route path='/projects' exact component={Projects} />
+				<Route path='/courses' exact component={Courses} />
+				<Route path='/contact' exact component={Contact} />
+			</BrowserRouter>
+		</div>
+  );
 }
