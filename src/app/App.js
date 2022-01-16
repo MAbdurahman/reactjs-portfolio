cc751ba072746f690a2b7483b93c70955a666e41
MAbdurahman <mdbdrrhm@gmail.com>
2022-01-16 05:22:53 -0600

    Add Switch and Error page to App

--- src/app/App.js
+++ src/app/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Home from './../pages/Home';
 import Contact from './../pages/Contact';
 import Courses from './../pages/Courses';
 import Projects from './../pages/Projects';
 import About from './../pages/About';
-
+import Error from './../pages/Error';
 
 export default function App() {
-  return (
+	return (
 		<div>
 			<BrowserRouter>
+			<Switch>
 				<Route path='/' exact component={Home} />
-        <Route path='/about' exact component={About} />
+				<Route path='/about' exact component={About} />
 				<Route path='/projects' exact component={Projects} />
 				<Route path='/courses' exact component={Courses} />
 				<Route path='/contact' exact component={Contact} />
+				<Route path='*' component={Error} />
+			</Switch>
 			</BrowserRouter>
 		</div>
-  );
+	);
 }
