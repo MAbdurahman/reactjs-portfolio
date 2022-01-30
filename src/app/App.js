b570333e34d8dbb2187f56625089b81e5553ea1c
MAbdurahman <mdbdrrhm@gmail.com>
2022-01-30 11:02:16 -0600

    Add html skeleton and basic style for About.jsx

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './../stylesheets/home.css';
 import './../stylesheets/header-footer.css'
+import './../stylesheets/about.css';
 import Preloader from '../components/Preloader';
 import Navigation from '../components/Navigation';
 import Home from './../pages/Home';
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,69 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 
 export default function About() {
    return (
-      <>
-      <section id='about'>
-         <h2>about page</h2>
-      </section>
-      </>
-   )
+		<>
+			<section id='about'>
+				<div className='container'>
+					<div className='row'>
+						<div className='col-lg-5 col-xs-12'>
+							<div className='about_image'>
+								<img src='./assets/about/author.jpg' alt='author' />
+							</div>
+						</div>
+						<div className='col-lg-7 col-xs-12'>
+							<div className='about_info'>
+								<div className='about_info--title'>
+									<h2>About Me</h2>
+								</div>
+								<div className='about_info--content'>
+									<div className='about-article'>
+										<p className='about-text'>
+											Hello!&nbsp;I am&nbsp;
+											<strong>Andre Romanovich</strong>, a Developer
+											and Instructor in Computer Science, living in
+											the northeast region of the United States. I
+											bring to the table eight years of experience in
+											software and web development, and three years
+											experience as a instructor in Computer Science,
+											where I regularly assisted and aided students,
+											managed and represented my company brand
+											through exemplary service.
+										</p>
+										<p className='about-text'>
+											Moreover, I have volunteered with my local
+											community center for over seven years mentoring
+											children and helping them with their homework,
+											raising money for community events and
+											assisting with maintenance projects. I am a
+											dedicated problem solver!
+										</p>
+                              <div className="about_info--button-container">
+										<button className='btn-default btn-home'>
+											<Link
+												activeClass='active'
+												to='skills'
+												spy={true}
+												smooth={true}
+												offset={0}
+												duration={500}
+											>
+												See More
+											</Link>
+										</button>
+                              <button className='btn-default btn-home'>
+                                 <a href="tel:9017538834">Call Me</a>
+                              </button>
+
+                              </div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 }
